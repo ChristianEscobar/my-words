bf05c036af48687852e1fe819f0746addc609df6
Christian Escobar <crs.escobar@gmail.com>
2018-02-19 20:39:21 -0800

    initial working definitions

--- app/public/assets/javascript/app.js
+++ app/public/assets/javascript/app.js
@@ -1,7 +1,8 @@
 $('#lookup-btn').on('click', function(event) {
   event.preventDefault();
 
-  $(".definition-container").hide();
+  clearPreviousResults();
+  $(".definition-display").hide();
 
   let word = $('#word-input').val().trim();
 
@@ -12,28 +13,57 @@ $('#lookup-btn').on('click', function(event) {
   $.ajax({
     url: "/api/lookup",
     method: "POST",
-    data: wordObj
+    contentType: "application/json",
+    data: JSON.stringify(wordObj)
   }).done((response) => {
     $("#word").text(word);
-    //$("#pronounciation").text(response.results[0].lexicalEntries[0].pronunciations[0].audioFile);
+    
+    const lexicalEntries = response.results[0].lexicalEntries;
 
-    for(let i=0; i<response.results[0].lexicalEntries.length; i++) {
-      $(".definition-container").append(response.results[0].lexicalEntries[i].lexicalCategory);
+    for(let i=0; i < lexicalEntries.length; i++) {
+      const entries = lexicalEntries[i].entries;
+
+      for(let j=0; j < entries.length; j++) {
+        const senses = entries[j].senses;
+
+        const ol_senses = $("<ol></ol>");
+        for(let k=0; k < senses.length; k++) {
+          senses[k].definitions.map((entry) => {
+            const li = $("<li></li>");
+            li.text(entry);
+
+            ol_senses.append(li);
+          });
+
+          // sub-senses
+          const subsenses = (senses[k].subsenses === undefined ? [] : senses[k].subsenses);
+
+          const ol_subsenses = $("<ol></ol>");
+
+          for(let l=0; l < subsenses.length; l++) {
+            subsenses[l].definitions.map((entry) => {
+              const li = $("<li></li>");
+              li.text(entry);
+
+              ol_subsenses.append(li);
+            });
+          }
+
+          ol_senses.append(ol_subsenses);
+        }
+
+        $(".definition-display").append(ol_senses);
+      }
     }
 
-    $(".definition-container").show();
+    $(".definition-display").show();
+
+  }).fail((error) => {
+    console.error(error);
   });
+
+  function clearPreviousResults() {
+    $("#word").empty();
+    $(".definition").empty();
+  }
 });
-
-/*
-function lookupWord(word) {
-  let apiURL = 'https://api.collinsdictionary.com/api/v1/dictionaries';
-
-  $.ajax({
-    url: apiURL,
-    method: 'GET'
-  }).done(function(response){
-    console.log(response);
-  });
-}
-*/
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
 	app.post("/api/lookup", (req, res) => {
 		const options = {
     	uri: "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" 
-            + req.body.word + "/definitions;pronunciations;examples",
+            + req.body.word + "/definitions;pronunciations",
     	headers: {
           "Accept": "application/json",
   				"app_id": "8d60399d",
@@ -15,23 +15,9 @@ module.exports = (app) => {
 
 	 	request(options)
 	 	.then((def) => {
-   		//console.log("def => ", def);
-
    		res.json(def);
 		}).catch((err) => {
    		console.error(err);
 		});
-
-
- 			/*
-  	$.ajax({
-    	url: apiURL,
-    	method: 'GET'
-  	}).done(function(response){
-    	console.log(response);
-
-    	res.json(true);
-  	});
-  	*/
 	});
 }
